de97062045bbe845ed12361d28fdd4314f8fc2bb
161250051 <m13236537528_1@163.com>
2018-05-24 00:44:24 +0800

    新增UserUtils工具

---
+++ WEB_CONTENT/src/assets/js/UserUtils.js
@@ -0,0 +1,22 @@
+/*
+	用戶工具類
+ */
+
+class UserUtils {
+	static isWorker(context) {
+		return context.$store.getters.getUserType != 'WORKER';
+	}
+
+	static isRequester(context) {
+		return context.$store.getters.getUserType != "REQUESTER";
+	}
+
+	static isAdmin(context) {
+		return context.$store.getters.getUserType != 'ADMIN';
+	}
+
+	constructor() {
+	}
+}
+
+export default UserUtils;
