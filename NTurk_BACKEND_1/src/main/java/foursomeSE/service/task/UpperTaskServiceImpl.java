8bd1c77a1b5084672ddc654a35ab1136e932b2d4
JiYuzhe <826468425@qq.com>
2018-06-18 23:34:20 +0800

    error fix

--- NTurk_BACKEND_1/src/main/java/foursomeSE/service/task/UpperTaskServiceImpl.java
+++ NTurk_BACKEND_1/src/main/java/foursomeSE/service/task/UpperTaskServiceImpl.java
@@ -473,8 +473,10 @@ public class UpperTaskServiceImpl implements UpperTaskService, MyConstants {
             for (Task t : wTs) {
                 long countPass = annotationJPA.countPassByTaskAndUser(t.getTaskId(), w.getEmailAddress());
                 long countFail = annotationJPA.countFailByTaskAndUser(t.getTaskId(), w.getEmailAddress());
-                Record record = new Record((int) w.getId(), (int) t.getTaskId(), (int) countPass, (int) (countFail + countPass));
-                records.add(record);
+                if (countPass + countFail != 0) {
+                    Record record = new Record((int) w.getId(), (int) t.getTaskId(), (int) countPass, (int) (countFail + countPass));
+                    records.add(record);
+                }
             }
         }
         return records;
